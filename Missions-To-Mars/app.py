4ee0cc1f4b8ca7800da7c5e964eb15a0dc0e80b8
Fereshteh Aghaei <fereshtehaghaei17@yahoo.com>
2020-12-06 21:28:58 -0700

    working on app.py

--- Missions-To-Mars/app.py
+++ Missions-To-Mars/app.py
@@ -1,6 +1,8 @@
+from webdriver_manager.chrome import ChromeDriverManager
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
+
 
 # Create an instance of Flask
 app = Flask(__name__)
